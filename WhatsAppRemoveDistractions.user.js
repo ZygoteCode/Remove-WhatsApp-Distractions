9194a599260336ec056851794f9abfcddded4fd1
ZygoteCode <exilephotographer@proton.me>
2025-06-07 20:00:01 +0200

    Update WhatsAppRemoveDistractions.user.js

--- WhatsAppRemoveDistractions.user.js
+++ WhatsAppRemoveDistractions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WhatsApp Remove Distractions
 // @namespace    http://tampermonkey.net/
-// @version      9.0
+// @version      10.0
 // @description  Remove all the WhatsApp distractions to get concentrated on what's really important
 // @author       ZygoteCode
 // @match        https://www.whatsapp.com/*
@@ -101,15 +101,6 @@
 
         try
         {
-            document.querySelector("path[d='M5.00847986,1.6 C6.38255462,1.6 7.50937014,2.67435859 7.5940156,4.02703389 L7.59911976,4.1906399 L7.599,5.462 L7.75719976,5.46214385 C8.34167974,5.46214385 8.81591972,5.94158383 8.81591972,6.53126381 L8.81591972,9.8834238 C8.81591972,10.4731038 8.34167974,10.9525438 7.75719976,10.9525438 L2.25767996,10.9525438 C1.67527998,10.9525438 1.2,10.4731038 1.2,9.8834238 L1.2,6.53126381 C1.2,5.94158383 1.67423998,5.46214385 2.25767996,5.46214385 L2.416,5.462 L2.41679995,4.1906399 C2.41679995,2.81636129 3.49135449,1.68973395 4.84478101,1.60510326 L5.00847986,1.6 Z M5.00847986,2.84799995 C4.31163824,2.84799995 3.73624912,3.38200845 3.6709675,4.06160439 L3.6647999,4.1906399 L3.663,5.462 L6.35,5.462 L6.35111981,4.1906399 C6.35111981,3.53817142 5.88169076,2.99180999 5.26310845,2.87228506 L5.13749818,2.85416626 L5.00847986,2.84799995 Z']").parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
-        }
-        catch (e)
-        {
-
-        }
-
-        try
-        {
             var elements = document.querySelectorAll("div[style='height: 49px; width: 49px;']");
 
             for (var i = 0; i < elements.length; i++)
@@ -138,9 +129,5 @@
         500);
     }
 
-    window.addEventListener('load', function () {
-      setTimeout(function () {
-          asyncLoop();
-      }, 2000);
-    });
+    asyncLoop();
 })();
